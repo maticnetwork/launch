3c15f343214182c98bda08f00ecfe42324ed114e
Jaynti Kanani <jdkanani@gmail.com>
2020-09-23 13:36:16 +0530

    fix: use new flags instead of deprecated flags

--- testnet-v4/sentry/sentry/bor/start.sh
+++ testnet-v4/sentry/sentry/bor/start.sh
@@ -7,12 +7,12 @@ DATA_DIR=$BOR_DIR/data
 
 bor --datadir $DATA_DIR \
   --port 30303 \
-  --rpc --rpcaddr '0.0.0.0' \
-  --rpcvhosts '*' \
-  --rpccorsdomain '*' \
-  --rpcport 8545 \
+  --http --http.addr '0.0.0.0' \
+  --http.vhosts '*' \
+  --http.corsdomain '*' \
+  --http.port 8545 \
   --ipcpath $DATA_DIR/bor.ipc \
-  --rpcapi 'db,eth,net,web3,txpool,bor' \
+  --http.api 'eth,net,web3,txpool,bor' \
   --syncmode 'full' \
   --networkid '80001' \
   --miner.gaslimit '200000000' \
@@ -23,4 +23,4 @@ bor --datadir $DATA_DIR \
   --txpool.lifetime '0h16m0s' \
   --maxpeers 200 \
   --metrics \
-  --pprof --pprofport 7071 --pprofaddr '0.0.0.0'
+  --pprof --pprof.port 7071 --pprof.addr '0.0.0.0'
