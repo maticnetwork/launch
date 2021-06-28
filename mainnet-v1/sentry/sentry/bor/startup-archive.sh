6ba222b1e2ddad05c12946c7731910ef68d90424
noam-alchemy <76969113+noam-alchemy@users.noreply.github.com>
2021-06-28 12:01:03 -0700

    create a startup script for archive nodes

---
+++ mainnet-v1/sentry/sentry/bor/startup-archive.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+set -x #echo on
+
+BOR_DIR=${BOR_DIR:-~/.bor}
+DATA_DIR=$BOR_DIR/data
+
+bor --datadir $DATA_DIR \
+  --port 30303 \
+  --http --http.addr '0.0.0.0' \
+  --http.vhosts '*' \
+  --http.corsdomain '*' \
+  --http.port 8545 \
+  --gcmode 'archive' \
+  --ipcpath $DATA_DIR/bor.ipc \
+  --http.api 'eth,net,web3,txpool,bor' \
+  --syncmode 'full' \
+  --networkid '137' \
+  --miner.gaslimit '20000000' \
+  --miner.gastarget '20000000' \
+  --txpool.nolocals \
+  --txpool.accountslots '128' \
+  --txpool.globalslots '20000'  \
+  --txpool.lifetime '0h16m0s' \
+  --maxpeers 200 \
+  --metrics \
+  --pprof --pprof.port 7071 --pprof.addr '0.0.0.0'
