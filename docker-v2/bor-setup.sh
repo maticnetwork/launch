45e9e24fbdb8c51e37ac90f1cb98c38c21cff40c
Arpit Temani <temaniarpit27@gmail.com>
2021-11-02 14:38:21 +0530

    modify files

--- docker-v2/bor-setup.sh
+++ docker-v2/bor-setup.sh
@@ -5,7 +5,7 @@ set -e
 ENV_FILE="$1.env"
 export $(xargs < $ENV_FILE)
 
-# rm -rf $BOR_DIR
+rm -rf $BOR_DIR
 mkdir -p $BOR_DIR
 
 cp ./bor /Users/arpittemani/.gvm/pkgsets/go1.15.1/global/bin/
@@ -14,17 +14,22 @@ cp ./bootnode /Users/arpittemani/.gvm/pkgsets/go1.15.1/global/bin/
 # cp ./bor /usr/local/bin/
 # cp ./bootnode /usr/local/bin/
 
-cp ../$NETWORK/sentry/sentry/bor/setup.sh ./
-cp ../$NETWORK/sentry/sentry/bor/genesis.json ./
-cp ../$NETWORK/sentry/sentry/bor/static-nodes.json ./
-cp ../$NETWORK/sentry/sentry/bor/start.sh ./
+cp ../$NETWORK-$VERSION/sentry/sentry/bor/setup.sh ./
+cp ../$NETWORK-$VERSION/sentry/sentry/bor/genesis.json ./
+cp ../$NETWORK-$VERSION/sentry/sentry/bor/static-nodes.json ./
+cp ../$NETWORK-$VERSION/sentry/sentry/bor/start.sh ./
 
 bash setup.sh
 
 # TODO - Check if snapshot is downloaded and applied
 
-wget -c $BOR_FULL_NODE_SNAPSHOT_URL -P $BOR_DIR
-tar -xzvf $BOR_DIR/$BOR_FULL_NODE_SNAPSHOT_FILE -C $BOR_DIR/.bor/data/bor/chaindata
+# wget -c $BOR_FULL_NODE_SNAPSHOT_URL -P $BOR_DIR
+# tar -xzvf $BOR_DIR/$BOR_FULL_NODE_SNAPSHOT_FILE -C $BOR_DIR/.bor/data/bor/chaindata
 
 sed -i "/datadir/a --bootnodes \"${BOR_BOOTNODES}\" \\\ " ./start.sh
-sed -i "/datadir/a --bor.heimdall http:\/\/heimdall0:1317 \\\ " ./start.sh
+sed -i "/datadir/a --bor.heimdall http:\/\/heimdall-rest-server0:1317 \\\ " ./start.sh
+
+cp ./setup.sh $BOR_DIR/
+cp ./genesis.json $BOR_DIR/
+cp ./static-nodes.json $BOR_DIR/
+cp ./start.sh $BOR_DIR/
--- docker-v2/heimdall-setup.sh
+++ docker-v2/heimdall-setup.sh
@@ -5,7 +5,7 @@ set -e
 ENV_FILE="$1.env"
 export $(xargs < $ENV_FILE)
 
-rm -rf $HEIMDALL_DIR/.heimdalld/config
+rm -rf $HEIMDALL_DIR
 mkdir -p $HEIMDALL_DIR
 
 # Init heimdall directory
@@ -14,10 +14,12 @@ mkdir -p $HEIMDALL_DIR
 # TODO - Check if snapshot is downloaded and applied
 
 # Download snapshot
-wget -c $HEIMDALL_SNAPSHOT_URL -P $HEIMDALL_DIR
-tar -xzvf $HEIMDALL_DIR/$HEIMDALL_SNAPSHOT_FILE -C $HEIMDALL_DIR/.heimdalld/data
+# wget -c $HEIMDALL_SNAPSHOT_URL -P $HEIMDALL_DIR
+# tar -xzvf $HEIMDALL_DIR/$HEIMDALL_SNAPSHOT_FILE -C $HEIMDALL_DIR/.heimdalld/data
 
-cp ../$NETWORK/sentry/sentry/heimdall/config/genesis.json $HEIMDALL_DIR/.heimdalld/config
+cp ../$NETWORK-$VERSION/sentry/sentry/heimdall/config/genesis.json $HEIMDALL_DIR/.heimdalld/config
 sed -i "s/seeds =.*/seeds = ${HEIMDALL_SEEDS}/" $HEIMDALL_DIR/.heimdalld/config/config.toml
 sed -i "s/127.0.0.1:26657/0.0.0.0:26657/" $HEIMDALL_DIR/.heimdalld/config/config.toml
 sed -i "s~http://localhost:8545~${HEIMDALL_BOR_RPC_URL}~" $HEIMDALL_DIR/.heimdalld/config/heimdall-config.toml
+
+cp heimdalld $HEIMDALL_DIR
