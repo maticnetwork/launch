c155d97288544269467db7df4f0cd8cabac46f80
rBurgett <ryan@burgettdev.net>
2022-03-20 13:36:24 -0400

    Removed deprecated static-nodes.json file

--- testnet-v4/without-sentry/bor/setup.sh
+++ testnet-v4/without-sentry/bor/setup.sh
@@ -12,9 +12,6 @@ mkdir -p $BOR_DIR $BOR_DIR/keystore
 # init bor
 bor --datadir $DATA_DIR init ./genesis.json
 
-# copy peers file
-cp ./static-nodes.json $DATA_DIR/bor/static-nodes.json
-
 # if node key not present, create nodekey
 if [ ! -f $NODE_KEY ]; then
   bootnode -genkey $NODE_KEY
